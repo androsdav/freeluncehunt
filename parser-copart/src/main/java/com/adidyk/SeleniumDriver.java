9d492612efae4084d160c89399fbc5a8209e3f48
androsdav <androsdav@gmail.com>
2021-02-09 18:22:44 +0200

    refactoring code

--- parser-copart/src/main/java/com/adidyk/SeleniumDriver.java
+++ parser-copart/src/main/java/com/adidyk/SeleniumDriver.java
@@ -15,8 +15,6 @@ public class SeleniumDriver {
 
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.copart.com/vehicleFinderSearch/?displayStr=%5B2010%20TO%202021%5D&from=%2FvehicleFinder%2F&searchStr=%7B%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23LotYear:%5B2010%20TO%202021%5D%22%5D,%22sortByZip%22:false,%22buyerEnteredZip%22:null,%22milesAway%22:null%7D&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22FETI%22:%5B%22buy_it_now_code:B1%22%5D,%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23LotYear:%5B2010%20TO%202021%5D%22%5D%7D,%22sort%22:%5B%22auction_date_type%20desc%22,%22auction_date_utc%20asc%22%5D,%22watchListOnly%22:false,%22searchName%22:%22%22,%22freeFormSearch%22:false%7D");
-
-        //WebElement table = new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.id("serverSideDataTable")));
         WebElement tbody = new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.tagName("tbody")));
         Thread.sleep(2000);
         List<WebElement> rows = tbody.findElements(By.tagName("tr"));
@@ -27,10 +25,6 @@ public class SeleniumDriver {
             String make = row.findElement(By.cssSelector("span[data-uname='lotsearchLotmake']")).getText();
             String model = row.findElement(By.cssSelector("span[data-uname='lotsearchLotmodel']")).getText();
             System.out.println("lot: " + lot + "; year: " + year + "; make: " + make + "; model: " + model);
-            //String lot = row.findElement(By.cssSelector("a.search-results")).getText();
-            //String year = row.findElement(By.name("data-uname")).getText();
-            //System.out.println("lot: " + lot);
-            //System.out.println("year: " + year);
         }
 
         System.out.println("sleep .......");
