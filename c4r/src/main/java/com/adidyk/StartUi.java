a0806ecb49598b6f6f85391929a810abd8409271
androsdav <androsdav@gmail.com>
2021-04-05 17:55:47 +0300

    new swing project

---
+++ c4r/src/main/java/com/adidyk/StartUi.java
@@ -0,0 +1,19 @@
+package com.adidyk;
+
+/**
+ * Class StartUi. Start project.
+ */
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class StartUi {
+
+     /**
+     * main - main.
+     * @param arg - arg.
+     */
+    public  static void main(String[] arg) {
+            System.out.println("Hello word !!!");
+
+    }
+
+}
